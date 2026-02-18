6dd2f154e9f111d07d3ecfef213d7838f35bd104
Юлия Кайко <pugaculia94@gmail.com>
2026-02-18 14:30:29 +0300

    corrected CI v.12

--- src/test/java/com/mymicroservice/orderservice/config/AbstractContainerTest.java
+++ src/test/java/com/mymicroservice/orderservice/config/AbstractContainerTest.java
@@ -6,7 +6,6 @@ import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.Duration;
