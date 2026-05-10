362b3d9d7373cacdbb0ed95aa97e2b2d050f993c
pugac <pugaculia94@gmail.com>
2026-05-10 20:17:23 +0300

    corrected outbox bags 2

--- src/main/java/com/mymicroservice/orderservice/model/Order.java
+++ src/main/java/com/mymicroservice/orderservice/model/Order.java
@@ -2,7 +2,16 @@ package com.mymicroservice.orderservice.model;
 
 import com.github.f4b6a3.uuid.UuidCreator;
 import com.mymicroservice.orderservice.model.enums.OrderStatus;
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
